081bb68862a358e8db08cf942a18a9744d78a0fd
Brayan-BH <brayangithub007@gmail.com>
2023-04-17 19:44:24 -0500

    Nueva Seccion de Conductor -Añadir estilos a la pagina conductor -Nuevos estilos a la pagina conductor

--- components/Conductor.vue
+++ components/Conductor.vue
@@ -1,5 +1,33 @@
 <template>
-   <div>
-    Seccion Conductor
-   </div>
+    <div class="conductor-container">
+        <div class="container-inner">
+            <div>
+                <h4>PARA CONDUCTORES</h4>
+                <h2>
+                    Le gustaria ganar con nosotros?
+                </h2>
+            </div>
+            <div class="wrapper">
+                <div class="wrapper_text">
+                    Quisque sollicitudin feugiat risus, eu posuere ex euismod eu. Phasellus hendrerit, massa efficitur dapibus
+                    pulvinar, sapien eros sodales ante, euismod aliquet nulla metus a mauris.
+                </div>
+                <div class="column">
+                    <div class="column-wrapper">
+                        <ul class="wrapper_items">
+                            <li>Luxury cars</li>
+                            <li>No fee</li>
+                            <li>Weekly payment</li>
+                            <li>Fixed price</li>
+                            <li>Good application</li>
+                            <li>Stable orders</li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="wrap">
+                    <a href="" class="btn-wrap">Become a Driver</a>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,4 +1,5 @@
 <template>
        <Intro />
        <Servicio />
+       <Conductor/>
 </template>
