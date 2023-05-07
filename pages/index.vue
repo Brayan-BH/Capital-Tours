0067f1feabd96af9df6621df338e2927f34a65d1
Brayan-BH <brayangithub007@gmail.com>
2023-05-06 20:31:09 -0500

    Update: Chat- bot -Se agrego el chat bot con tawk.to

--- pages/index.vue
+++ pages/index.vue
@@ -2,6 +2,23 @@
        <Intro />
        <Servicio />
        <Conductor />
-       <App/>
+       <App />
        <Footer />
 </template>
+<!--Start of Tawk.to Script-->
+<script >
+       export default {
+              mounted() {
+                     var Tawk_API = Tawk_API || {}, Tawk_LoadStart = new Date();
+                     (function () {
+                            var s1 = document.createElement("script"), s0 = document.getElementsByTagName("script")[0];
+                            s1.async = true;
+                            s1.src = 'https://embed.tawk.to/6456f01d6a9aad4bc5794e4a/1gvppjt20';
+                            s1.charset = 'UTF-8';
+                            s1.setAttribute('crossorigin', '*');
+                            s0.parentNode.insertBefore(s1, s0);
+                     })();
+              }
+       };
+</script>
+<!--End of Tawk.to Script-->
